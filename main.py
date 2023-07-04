116e1fb813c6ad4da5be0e0a79bb734c563b1ca8
lucasri-silva <lucasrsilva.dev@gmail.com>
2023-07-04 01:20:25 -0300

    first commit

--- main.py
+++ main.py
@@ -10,178 +10,198 @@ import floyd_warshall
 import graph_generator
 import plot_result
 
+def clear_terminal():
+    print("\033c", end="")
+
 def main():
-    shutil.rmtree('./result/')
-    os.makedirs('./result/')
-    os.makedirs('./result/positive-weight')
-    os.makedirs('./result/positive-negative-weight')
+    answer = 1
+    while answer != 4:
+        print('1 - Generate new graphs')
+        print('2 - Execute Dijkstra, Bellman Ford and Floyd Warshall')
+        print('3 - Plot result')
+        print('4 - End program')
+        answer = input('Enter an option: ')
+
+        if not answer.isdigit():
+            print('Invalid option')
+
+        else:
+            answer = int(answer)
+            if answer == 1:
+
+                shutil.rmtree('./result/')
+                os.makedirs('./result/')
+                os.makedirs('./result/positive-weight')
+                os.makedirs('./result/positive-negative-weight')
+
+                print('Gerando grafos ...')
+                shutil.rmtree('./graphs')
+                os.makedirs('./graphs/positive-weight')
+                os.makedirs('./graphs/positive-negative-weight')
+
+                num_graphs = 1
+                pace = 25
+
+                ## generate positive weight graphs
+                num_nodes = 10
+                num_edges = num_nodes * (num_nodes - 1)
+                num_max_nodes = 500
+                densities = [1.0, 0.9, 0.6, 0.5]
+                weight_range = (1, 100)
+                path = 'positive-weight'
+
+                # for density in densities:
+
+                graph_generator.create_graphs(num_nodes, num_max_nodes, num_edges, weight_range, density, num_graphs, pace, path)
+
+                ## generate positive-negative weight graphs
+                num_nodes = 5
+                num_max_nodes = 50
+                densities = [0.5]
+                weight_range = (-30, 100)
+                path = 'positive-negative-weight'
+
+                for density in densities:
+
+                    graph_generator.create_graphs(num_nodes, num_max_nodes, num_edges, weight_range, density, num_graphs, pace, path)
+
+                input('All good. Press any key to continue')
+                clear_terminal()
+
+            elif answer == 2:
+
+                print('Calculando caminho mínimo ...')
+                # iterate over all files in directory positive-weight
+                for filename in os.listdir('./graphs/positive-weight/'):
+                    file_path = os.path.join('./graphs/positive-weight/', filename)
+                    graph_qtd_nodes_num = filename.split('_')[0]  
+                    graph_density = filename.split('_')[1][:3]
+
+                    if os.path.isfile(file_path):
+                        if graph_density == '1.0' or graph_density == '0.9':
+                            graph_density = 'dense'
+                        elif graph_density == '0.6' or graph_density == '0.5':
+                            graph_density = 'sparse'
+
+                        result_file_dijkstra = open('./result/positive-weight/'+'dijkstra_'+graph_density+'.csv', 'a')
+                        result_file_bellman = open('./result/positive-weight/'+'bellman_'+graph_density+'.csv', 'a')
+                        result_file_floyd = open('./result/positive-weight/'+'floyd_'+graph_density+'.csv', 'a')
+                        with open(file_path, 'r') as file:
+
+                            for line in file:
+
+                                graph = json.loads(line)
+                                start_vertex = random.randint(1, len(graph))
+                                end_vertex = random.randint(1, len(graph))
+
+                                while end_vertex == start_vertex:
+                                    start_vertex = random.randint(1, len(graph))
+                                    end_vertex = random.randint(1, len(graph))
+
+                                
+                                ## DIJKSTRA
+                                start_time = time.time()
+                                distances, predecessors = dijkstra.dijkstra(graph, start_vertex)
+                                # dijkstra.dijkstra_get_path(start_vertex, end_vertex, predecessors)
+                                # dijkstra.dijkstra_get_cost(start_vertex, end_vertex, distances)
+                                end_time = time.time()
+                                elapsed_time = end_time - start_time
+                                print(f"Execution time: {elapsed_time} seconds")
+                                result_file_dijkstra.write(graph_qtd_nodes_num+','+str(elapsed_time))
+                                result_file_dijkstra.write('\n')
+
+                                ## BELLMAN FORD 
+                                start_time = time.time()
+                                distances, predecessors, counter, negative_cycle = bellman_ford.bellman_ford(graph, start_vertex)
+                                # bellman_ford.bellman_ford_get_path(start_vertex, end_vertex, predecessors)
+                                # bellman_ford.bellman_ford_get_cost(start_vertex, end_vertex, distances)
+                                end_time = time.time()
+                                elapsed_time = end_time - start_time
+                                print(f"Execution time: {elapsed_time} seconds")
+                                result_file_bellman.write(graph_qtd_nodes_num+','+str(elapsed_time))
+                                result_file_bellman.write('\n')
+
+                                ## FLOYD WARSHALL
+                                start_time = time.time()
+                                distances, predecessors = floyd_warshall.floyd_warshall(graph)
+                                end_time = time.time()
+                                # floyd_warshall.floyd_warshall_get_path(start_vertex, end_vertex, predecessors, distances)
+                                elapsed_time = end_time - start_time
+                                print(f"Execution time: {elapsed_time} seconds")
+                                result_file_floyd.write(graph_qtd_nodes_num+','+str(elapsed_time))
+                                result_file_floyd.write('\n')
 
 
-    answer = input('Do you want to generate new graphs? (y/n) ')
+                            result_file_dijkstra.close()
+                            result_file_bellman.close()
+                            result_file_floyd.close()
+                            file.close()
 
-    if answer == 'y':
-        print('Gerando grafos ...')
-        shutil.rmtree('./graphs')
-        os.makedirs('./graphs/positive-weight')
-        os.makedirs('./graphs/positive-negative-weight')
+                # iterate over all files in directory positive-negative-weight
+                for filename in os.listdir('./graphs/positive-negative-weight/'):
+                    file_path = os.path.join('./graphs/positive-negative-weight/', filename)
+                    result_file_bellman = open('./result/positive-negative-weight/'+'bellman'+'.csv', 'a')
+                    result_file_floyd = open('./result/positive-negative-weight/'+'floyd'+'.csv', 'a')
+                    graph_qtd_nodes_num = filename.split('_')[0]  
 
-        num_graphs = 10
-        pace = 25
+                    if os.path.isfile(file_path):
 
-        ## generate positive weight graphs
-        num_nodes = 10
-        num_edges = num_nodes * (num_nodes - 1)
-        num_max_nodes = 100
-        densities = [1.0, 0.9, 0.6, 0.5]
-        weight_range = (1, 100)
-        path = 'positive-weight'
-        for density in densities:
+                        with open(file_path, 'r') as file:
 
-            graph_generator.create_graphs(num_nodes, num_max_nodes, num_edges, weight_range, density, num_graphs, pace, path)
+                            for line in file:
 
-        ## generate positive-negative weight graphs
-        num_nodes = 5
-        num_max_nodes = 8
-        densities = [0.5]
-        weight_range = (-30, 100)
-        path = 'positive-negative-weight'
-        for density in densities:
+                                graph = json.loads(line)
+                                start_vertex = random.randint(1, len(graph))
+                                end_vertex = random.randint(1, len(graph))
 
-            graph_generator.create_graphs(num_nodes, num_max_nodes, num_edges, weight_range, density, num_graphs, pace, path)
+                                while end_vertex == start_vertex:
+                                    start_vertex = random.randint(1, len(graph))
+                                    end_vertex = random.randint(1, len(graph))
 
+                                # print(start_vertex)
+                                # print(end_vertex)
+                                
+                                ## BELLMAN FORD 
+                                start_time = time.time()
+                                distances, predecessors, counter, negative_cycle = bellman_ford.bellman_ford(graph, start_vertex)
+                                if negative_cycle:
+                                    print('negative cycle detected')
+                                elif str(distances[end_vertex-1]) == 'inf':
+                                    print('there is no path between start and end node')
+                                else:
+                                    # bellman_ford.bellman_ford_get_path(start_vertex, end_vertex, predecessors)
+                                    # bellman_ford.bellman_ford_get_cost(start_vertex, end_vertex, distances)
+                                    end_time = time.time()
+                                    elapsed_time = end_time - start_time
+                                    print(f"Execution time: {elapsed_time} seconds")
+                                    result_file_bellman.write(graph_qtd_nodes_num+','+str(elapsed_time))
+                                    result_file_bellman.write('\n')
+                                    # input('press')
 
-    print('Calculando caminho mínimo ...')
-    # iterate over all files in directory positive-weight
-    for filename in os.listdir('./graphs/positive-weight/'):
-        file_path = os.path.join('./graphs/positive-weight/', filename)
-        graph_qtd_nodes = filename.split('_')[0]
-        graph_qtd_nodes_num = filename.split('_')[0]  
-        graph_density = filename.split('_')[1][:3]
+                                    ## FLOYD WARSHALL
+                                    start_time = time.time()
+                                    distances, predecessors = floyd_warshall.floyd_warshall(graph)
+                                    end_time = time.time()
+                                    # floyd_warshall.floyd_warshall_get_path(start_vertex, end_vertex, predecessors, distances)
+                                    elapsed_time = end_time - start_time
+                                    print(f"Execution time: {elapsed_time} seconds")
+                                    result_file_floyd.write(graph_qtd_nodes_num+','+str(elapsed_time))
+                                    result_file_floyd.write('\n')
+                                    # input('press')
 
-        if os.path.isfile(file_path):
-            if graph_density == '1.0' or graph_density == '0.9':
-                graph_density = 'dense'
-            elif graph_density == '0.6' or graph_density == '0.5':
-                graph_density = 'sparse'
+                            file.close()
 
-            if int(graph_qtd_nodes) >= 70:
-                graph_qtd_nodes = 'big'
-            else:
-                graph_qtd_nodes = 'small'
-
-            result_file_dijkstra = open('./result/positive-weight/'+'dijkstra_'+graph_qtd_nodes+'_'+graph_density, 'a')
-            result_file_bellman = open('./result/positive-weight/'+'bellman_'+graph_qtd_nodes+'_'+graph_density, 'a')
-            result_file_floyd = open('./result/positive-weight/'+'floyd_'+graph_qtd_nodes+'_'+graph_density, 'a')
-            with open(file_path, 'r') as file:
-
-                for line in file:
-
-                    graph = json.loads(line)
-                    start_vertex = random.randint(1, len(graph))
-                    end_vertex = random.randint(1, len(graph))
-
-                    while end_vertex == start_vertex:
-                        start_vertex = random.randint(1, len(graph))
-                        end_vertex = random.randint(1, len(graph))
-
-                    
-                    ## DIJKSTRA
-                    start_time = time.time()
-                    distances, predecessors = dijkstra.dijkstra(graph, start_vertex)
-                    # dijkstra.dijkstra_get_path(start_vertex, end_vertex, predecessors)
-                    # dijkstra.dijkstra_get_cost(start_vertex, end_vertex, distances)
-                    end_time = time.time()
-                    elapsed_time = end_time - start_time
-                    print(f"Execution time: {elapsed_time} seconds")
-                    result_file_dijkstra.write(graph_qtd_nodes_num+','+str(elapsed_time))
-                    result_file_dijkstra.write('\n')
-
-                    ## BELLMAN FORD 
-                    start_time = time.time()
-                    distances, predecessors, counter, negative_cycle = bellman_ford.bellman_ford(graph, start_vertex)
-                    # bellman_ford.bellman_ford_get_path(start_vertex, end_vertex, predecessors)
-                    # bellman_ford.bellman_ford_get_cost(start_vertex, end_vertex, distances)
-                    end_time = time.time()
-                    elapsed_time = end_time - start_time
-                    print(f"Execution time: {elapsed_time} seconds")
-                    result_file_bellman.write(graph_qtd_nodes_num+','+str(elapsed_time))
-                    result_file_bellman.write('\n')
-
-                    ## FLOYD WARSHALL
-                    start_time = time.time()
-                    distances, predecessors = floyd_warshall.floyd_warshall(graph)
-                    end_time = time.time()
-                    # floyd_warshall.floyd_warshall_get_path(start_vertex, end_vertex, predecessors, distances)
-                    elapsed_time = end_time - start_time
-                    print(f"Execution time: {elapsed_time} seconds")
-                    result_file_floyd.write(graph_qtd_nodes_num+','+str(elapsed_time))
-                    result_file_floyd.write('\n')
-
-
-                result_file_dijkstra.close()
                 result_file_bellman.close()
                 result_file_floyd.close()
-                file.close()
+                input('All good. Press any key to continue')
+                clear_terminal()
 
-    # iterate over all files in directory positive-negative-weight
-    for filename in os.listdir('./graphs/positive-negative-weight/'):
-        file_path = os.path.join('./graphs/positive-negative-weight/', filename)
-        result_file_bellman = open('./result/positive-negative-weight/'+'bellman', 'a')
-        result_file_floyd = open('./result/positive-negative-weight/'+'floyd', 'a')
+            elif answer == 3:
 
-        if os.path.isfile(file_path):
+                plot_result.plot()
+                input('All good. Press any key to continue')
+                clear_terminal()
 
-            with open(file_path, 'r') as file:
-
-                for line in file:
-
-                    graph = json.loads(line)
-                    start_vertex = random.randint(1, len(graph))
-                    end_vertex = random.randint(1, len(graph))
-
-                    while end_vertex == start_vertex:
-                        start_vertex = random.randint(1, len(graph))
-                        end_vertex = random.randint(1, len(graph))
-
-                    # print(start_vertex)
-                    # print(end_vertex)
-                    
-                    ## BELLMAN FORD 
-                    start_time = time.time()
-                    distances, predecessors, counter, negative_cycle = bellman_ford.bellman_ford(graph, start_vertex)
-                    if negative_cycle:
-                        print('negative cycle detected')
-                    elif str(distances[end_vertex-1]) == 'inf':
-                        print('there is no path between start and end node')
-                    else:
-                        # bellman_ford.bellman_ford_get_path(start_vertex, end_vertex, predecessors)
-                        # bellman_ford.bellman_ford_get_cost(start_vertex, end_vertex, distances)
-                        end_time = time.time()
-                        elapsed_time = end_time - start_time
-                        print(f"Execution time: {elapsed_time} seconds")
-                        result_file_bellman.write(str(elapsed_time))
-                        result_file_bellman.write('\n')
-                        # input('press')
-
-                        ## FLOYD WARSHALL
-                        start_time = time.time()
-                        distances, predecessors = floyd_warshall.floyd_warshall(graph)
-                        end_time = time.time()
-                        # floyd_warshall.floyd_warshall_get_path(start_vertex, end_vertex, predecessors, distances)
-                        elapsed_time = end_time - start_time
-                        print(f"Execution time: {elapsed_time} seconds")
-                        result_file_floyd.write(str(elapsed_time))
-                        result_file_floyd.write('\n')
-                        # input('press')
-
-                file.close()
-
-    result_file_bellman.close()
-    result_file_floyd.close()
-
-    # answer = input('Do you want to plot the result? (y/n) ')
-    # if answer == 'y':
-    #     plot_result.plot()        
 
 if __name__ == "__main__":
     main()
--- plot_result.py
+++ plot_result.py
@@ -1,17 +1,35 @@
-# import matplotlib.pyplot as plt
-#
-# x = []
-# y = []
-#
-# def plot():
-#     with open("./result/positive-weight/dijkstra_big_dense", "r") as file:
-#         for line in file:
-#             values = line.strip().split(",")
-#             print(values)
-#             x.append(float(values[1]))
-#             y.append(float(values[0]))
-#
-#     plt.plot(x, y)
-#     plt.xlabel('Time (s)')
-#     plt.ylabel('Number of nodes')
-#     plt.show()
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def plot():
+    file_names = ['dijkstra_sparse.csv', 'bellman_sparse.csv', 'floyd_sparse.csv']
+    # file_names = ['dijkstra_sparse.csv', 'bellman_sparse.csv']
+    # file_names = ['dijkstra_dense.csv', 'bellman_dense.csv', 'floyd_dense.csv']
+    # file_names = ['dijkstra_dense.csv', 'bellman_dense.csv']
+
+
+    colors = {'dijkstra': 'red', 'bellman': 'green', 'floyd': 'blue'}
+
+    dataframes = []
+
+    # path = './result/positive-weight/'
+    path = './result/positive-negative-weight/'
+    for file in file_names:
+        df = pd.read_csv(path+file)
+        dataframes.append(df)
+
+    for i, df in enumerate(dataframes):
+        algorithm = file_names[i].split('_')[0]
+        algorithm_color = colors[algorithm]
+        x_column = df.columns[1]
+        y_column = df.columns[0]
+        plt.scatter(df[x_column], df[y_column], color=algorithm_color, label=algorithm, s=5)
+
+    plt.xlabel('Time (s)')
+    plt.ylabel('Number of nodes')
+    plt.title('Positive-weight sparse graphs Dijkstra x Bellman-Ford x Floyd-Warshall')
+    # plt.title('Positive-weight sparse graphs Dijkstra x Bellman-Ford')
+    # plt.title('Positive-weight dense graphs Dijkstra x Bellman-Ford x Floyd-Warshall')
+    # plt.title('Positive-weight dense graphs Dijkstra x Bellman-Ford')
+    plt.legend()
+    plt.show()
